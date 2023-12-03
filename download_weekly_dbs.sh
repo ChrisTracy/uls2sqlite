7a5d6e20f14c2e7cbbe2f1415d6cce0062bf2505
Chris Tracy <58871574+ChrisTracy@users.noreply.github.com>
2023-12-03 14:30:34 -0600

    restructure

--- download_weekly_dbs.sh
+++ download_weekly_dbs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Create the archives directory if it doesn't exist
-mkdir -p ../archives
+mkdir -p ./archives
 
 # Assuming weekly_db_urls.txt is now in the config folder
-config_folder="../config"
+config_folder="./config"
 url_file="$config_folder/weekly_db_urls.txt"
 
 for url in $(cat "$url_file"); do
@@ -12,7 +12,7 @@ for url in $(cat "$url_file"); do
     filename=$(basename "$url")
 
     # Download the file to the archives sub-directory using curl
-    curl -s -S -L --progress-bar "$url" -o "../archives/$filename"
+    curl -s -S -L --progress-bar "$url" -o "./archives/$filename"
 
     # Sleep for a random time between 30 seconds and 120 seconds (2 minutes)
     sleep $((30 + RANDOM % 91))
