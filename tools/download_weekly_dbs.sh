63dd382eef66e0a63f07029f6c43d8dd95b2f2ed
Chris Tracy <58871574+ChrisTracy@users.noreply.github.com>
2023-12-03 11:32:44 -0600

    Update download_weekly_dbs.sh

--- tools/download_weekly_dbs.sh
+++ tools/download_weekly_dbs.sh
@@ -4,7 +4,7 @@
 mkdir -p ../archives
 
 # Assuming weekly_db_urls.txt is now in the config folder
-config_folder="./config"
+config_folder="../config"
 url_file="$config_folder/weekly_db_urls.txt"
 
 for url in $(cat "$url_file"); do
