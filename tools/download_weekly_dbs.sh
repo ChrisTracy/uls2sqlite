8cbb53afe8de062522115cc7fe8b406eda74fd4f
Chris Tracy <58871574+ChrisTracy@users.noreply.github.com>
2023-12-03 11:26:11 -0600

    Update download_weekly_dbs.sh

--- tools/download_weekly_dbs.sh
+++ tools/download_weekly_dbs.sh
@@ -3,21 +3,21 @@
 # Create the archives directory if it doesn't exist
 mkdir -p ../archives
 
-for url in $(cat weekly_db_urls.txt); do
+# Assuming weekly_db_urls.txt is now in the config folder
+config_folder="../config"
+url_file="$config_folder/weekly_db_urls.txt"
+
+for url in $(cat "$url_file"); do
     # Extract the filename from the URL
     filename=$(basename "$url")
 
     # Download the file to the archives sub-directory using curl
-    # -s: Silent mode, don't show progress or error messages
-    # -S: Show error messages if an error occurs
-    # -L: Follow redirects
-    # -o: Specify output file
-    # --progress-bar: Show download progress
     curl -s -S -L --progress-bar "$url" -o "../archives/$filename"
 
     # Sleep for a random time between 30 seconds and 120 seconds (2 minutes)
     sleep $((30 + RANDOM % 91))
+done
 
+# Process Zip Files after all downloads are completed
 ./process_zip_files.sh
 
-done
