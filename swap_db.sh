2f5fd650ed5d5b84a9ae16090f0228ab04c591e4
Chris Tracy <58871574+ChrisTracy@users.noreply.github.com>
2023-12-12 00:06:56 -0600

    Only the new file needs to be verified

--- swap_db.sh
+++ swap_db.sh
@@ -12,34 +12,29 @@ if [ ! -d "$folder_path" ]; then
     exit 1
 fi
 
-# Check if the db.sqlite file exists and its size is at least 700MB
+# Check if the db.sqlite file exists
 db_sqlite="$folder_path/db.sqlite"
 random_sqlite=$(find "$folder_path" -maxdepth 1 -type f -name "*.sqlite" ! -name "db.sqlite" | head -n 1)
-
-if [ -f "$db_sqlite" ] && [ "$(stat -c%s "$db_sqlite")" -ge 700000000 ]; then
-    # Rename db.sqlite to [todays_date].sqlite
+    
+# Check if the random file exists and its size is at least 700MB
+if [ -n "$random_sqlite" ] && [ "$(stat -c%s "$random_sqlite")" -ge 700000000 ]; then
+    # Rename the random file to db.sqlite
     mv "$db_sqlite" "$folder_path/$today_date.sqlite"
+    mv "$random_sqlite" "$folder_path/db.sqlite"
+    echo "Random sqlite file renamed to db.sqlite."
     
-    # Check if the random file exists and its size is at least 700MB
-    if [ -n "$random_sqlite" ] && [ "$(stat -c%s "$random_sqlite")" -ge 700000000 ]; then
-        # Rename the random file to db.sqlite
-        mv "$random_sqlite" "$folder_path/db.sqlite"
-        echo "Random sqlite file renamed to db.sqlite."
-        
-        # Create the archive subfolder if it doesn't exist
-        archive_folder="$folder_path/archive"
-        mkdir -p "$archive_folder"
+    # Create the archive subfolder if it doesn't exist
+    archive_folder="$folder_path/archive"
+    mkdir -p "$archive_folder"
 
-        # Move the renamed db.sqlite to archive
-        mv "$folder_path/$today_date.sqlite" "$archive_folder/"
-        echo "Renamed db.sqlite moved to archive as $today_date.sqlite."
-    else
-        echo "Error: No valid random sqlite file found or its size is less than 700MB."
-        exit 1
-    fi
-    
-    echo "Files renamed successfully."
+    # Move the renamed db.sqlite to archive
+    mv "$folder_path/$today_date.sqlite" "$archive_folder/"
+    echo "Renamed db.sqlite moved to archive as $today_date.sqlite."
 else
-    echo "Error: db.sqlite does not exist or its size is less than 700MB."
+    echo "Error: No valid random sqlite file found or its size is less than 700MB."
     exit 1
+fi
+
+echo "Files renamed successfully."
+
 fi
