6ae61de8ab90bac352b78f970c1d371b36d595bf
Chris Tracy <58871574+ChrisTracy@users.noreply.github.com>
2023-12-12 00:50:42 -0600

    Update swap_db.sh

--- swap_db.sh
+++ swap_db.sh
@@ -3,21 +3,23 @@
 # Set the path to the folder
 folder_path="/databases"
 
-# Get today's date in the format YYYY-MM-DD
-today_date=$(date +"%Y-%m-%d")
-
 # Check if the folder exists
 if [ ! -d "$folder_path" ]; then
     echo "Error: Folder $folder_path not found."
     exit 1
 fi
 
-# Check if the db.sqlite file exists
-db_sqlite="$folder_path/db.sqlite"
+# Look for the random sqlite file exists
 random_sqlite=$(find "$folder_path" -maxdepth 1 -type f -name "*.sqlite" ! -name "db.sqlite" | head -n 1)
     
 # Check if the random file exists and its size is at least 700MB
 if [ -n "$random_sqlite" ] && [ "$(stat -c%s "$random_sqlite")" -ge 700000000 ]; then
+
+    db_sqlite="$folder_path/db.sqlite"
+    
+    # Get today's date in the format YYYY-MM-DD
+    today_date=$(date +"%Y-%m-%d")
+    
     # Rename the random file to db.sqlite
     mv "$db_sqlite" "$folder_path/$today_date.sqlite"
     mv "$random_sqlite" "$folder_path/db.sqlite"
